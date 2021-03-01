384ff1e79d7cbf21154d4f3285f097bb866c98f7
Sawant Nilesh <SawantNilesh@JohnDeere.com>
2021-03-01 16:01:08 +0530

    changes

--- src/com/nsawant77/practice/interfaces/InterfaceA.java
+++ src/com/nsawant77/practice/interfaces/InterfaceA.java
@@ -1,5 +1,7 @@
 package com.nsawant77.practice.interfaces;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 //If you know the full implementation
 public interface InterfaceA {
 
@@ -8,10 +10,12 @@ public interface InterfaceA {
     //must need to initialized
     int x = 0;
 
-
     //public abstract by default
     //cannot be private, protected, strictfp, synchronized, native
     void getData();
 
     public abstract String getValue();
+
+    //cannot have a static block
+    //cannot have a constructor
 }
